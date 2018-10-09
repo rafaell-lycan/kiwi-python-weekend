73cfda9acbbf6e2143ca274fba02785ed5226784
Rafaell Lycan <sonny.webdsg@gmail.com>
2018-10-09 12:46:14 +0200

    Check if it is a main module name

--- find_combinations.py
+++ find_combinations.py
@@ -1,25 +1,35 @@
+"""
+Find Combinations
+"""
+
 from route import Route
 from utils import parse_flight, add_flight, display_flights, load_flights
 
-flights = []
-routes = []
+# main program
+def main():
+  flights = []
+  routes = []
 
-lines = load_flights()
+  lines = load_flights()
 
-for line in lines:
-  flights.append(parse_flight(line))
+  for line in lines:
+    flights.append(parse_flight(line))
 
-for flight in flights:
-  flight.add_connections(flights)
+  for flight in flights:
+    flight.add_connections(flights)
 
-for flight in flights:
-  if len(flight.connections) != 0:
-    routes.append(Route(flight))
+  for flight in flights:
+    if len(flight.connections) != 0:
+      routes.append(Route(flight))
 
-for i in range(len(routes)):
-  add_flight(routes, i)
+  for i in range(len(routes)):
+    add_flight(routes, i)
 
-#print bags
-display_flights(routes, 0)
-display_flights(routes, 1)
-display_flights(routes, 2)
+  #print bags
+  display_flights(routes, 0)
+  display_flights(routes, 1)
+  display_flights(routes, 2)
+
+# if main module
+if __name__ == "__main__":
+  main()
